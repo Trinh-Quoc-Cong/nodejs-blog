2867f99b492e739f9c06642d4f44b2dc86327f0b
Trinh-Quoc-Cong <cong.tq194495@sis.hust.edu.vn>
2023-04-13 10:03:05 +0700

    bootstrap

---
+++ src/index.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const path = require('path')
+const morgan = require('morgan')
+const handlebars = require('express-handlebars')
+const app = express()
+
+const port = 3000
+// import { create } from 'express-handlebars';
+
+app.use(express.static(path.join(__dirname,'public')))
+app.use(morgan('combined'))
+app.engine('hbs', handlebars.engine({
+    extname: '.hbs'
+}))
+app.set('view engine', 'hbs')
+app.set('views', path.join(__dirname, 'resources','views'))
+console.log(__dirname);
+
+// get định nghĩa route
+app.get('/', (req, res) => {
+    res.render('home')
+})
+app.get('/news', (req, res) => {
+    res.render('news')
+})
+// localhost 127.0.0.1 
+app.listen(port, () => {
+    console.log(`Example app listening on port http://localhost:${port}`)
+})
