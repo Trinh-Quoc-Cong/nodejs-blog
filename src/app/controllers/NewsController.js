67aafe3fea8db870f39977c08849c6661037da57
Trinh-Quoc-Cong <cong.tq194495@sis.hust.edu.vn>
2023-04-17 16:06:58 +0700

    install prettier lint-staged husky

---
+++ src/app/controllers/NewsController.js
@@ -0,0 +1,13 @@
+class NewsControler {
+    // GET / news
+    index(req, res) {
+        res.render('news');
+    }
+
+    // GET /news/:slug
+    show(req, res) {
+        res.send('news detail!!!');
+    }
+}
+
+module.exports = new NewsControler();
--- src/index.js
+++ src/index.js
@@ -1,29 +1,36 @@
-const express = require('express')
-const path = require('path')
-const morgan = require('morgan')
-const handlebars = require('express-handlebars')
-const app = express()
+const express = require('express');
+const path = require('path');
+const morgan = require('morgan');
+const handlebars = require('express-handlebars');
+const app = express();
 
-const port = 3000
+const port = 3000;
 // import { create } from 'express-handlebars';
 
-app.use(express.static(path.join(__dirname,'public')))
-app.use(morgan('combined'))
-app.engine('hbs', handlebars.engine({
-    extname: '.hbs'
-}))
-app.set('view engine', 'hbs')
-app.set('views', path.join(__dirname, 'resources','views'))
-console.log(__dirname);
+const route = require('./routes');
 
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.urlencoded());
+app.use(express.json());
+app.use(morgan('combined'));
+app.engine(
+    'hbs',
+    handlebars.engine({
+        extname: '.hbs',
+    }),
+);
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources', 'views'));
+// console.log(__dirname);
+
+// Home , search , contact  // file Site
+
+// route init
+route(app);
 // get định nghĩa route
-app.get('/', (req, res) => {
-    res.render('home')
-})
-app.get('/news', (req, res) => {
-    res.render('news')
-})
-// localhost 127.0.0.1 
+
+// localhost 127.0.0.1
 app.listen(port, () => {
-    console.log(`Example app listening on port http://localhost:${port}`)
-})
+    console.log(`Example app listening on port http://localhost:${port}`);
+});
